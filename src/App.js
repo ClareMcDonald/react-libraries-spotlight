b8dd38ec63ccbcd2079eba989159df99cab7edbf
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-21 15:26:44 -0800

    installed and imported Victory. Default bar graph displays

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import MaterialTable from 'material-table';
 import data from './data';
 import { generateFields } from './data-utils';
+import reactDOM from 'react-dom';
+import * as V from 'victory';
+import { VictoryBar } from 'victory';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MaterialTable
-        // columns={[
-        //   { title: 'First Name', field: 'first_name' }
-        // ]}
-        columns={generateFields(data)}
-        data={data}
-        title="Clare's Cool Material-Table Table" 
-      />
+      <div className="table">
+        <MaterialTable
+          columns={generateFields(data)}
+          data={data}
+          title="Clare's Cool Material-Table Table" 
+        />
+      </div>
+      <div>
+        <VictoryBar />
+      </div>
     </div>
   );
 }
